6097e64d62310ce122cde273c95e221deb91e76e
abderrezzaq laanoui <laanoui.fst@uhp.ac.ma>
2020-11-27 16:54:53 +0100

    update resources

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,20 +29,24 @@ namespace Puzzle
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Puzzle));
             this.board = new System.Windows.Forms.TableLayoutPanel();
-            this.title = new System.Windows.Forms.Label();
-            this.timer = new System.Windows.Forms.Label();
-            this.button = new System.Windows.Forms.Button();
-            this.panel = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.clock = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.end = new System.Windows.Forms.Button();
+            this.modes = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
             this.board.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,14 +57,14 @@ namespace Puzzle
             this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
             this.board.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
-            this.board.Controls.Add(this.button8, 1, 1);
             this.board.Controls.Add(this.button7, 0, 2);
-            this.board.Controls.Add(this.button6, 0, 0);
-            this.board.Controls.Add(this.button5, 2, 2);
-            this.board.Controls.Add(this.button4, 1, 1);
-            this.board.Controls.Add(this.button3, 2, 0);
-            this.board.Controls.Add(this.button2, 1, 0);
             this.board.Controls.Add(this.button1, 0, 0);
+            this.board.Controls.Add(this.button3, 2, 0);
+            this.board.Controls.Add(this.button4, 0, 1);
+            this.board.Controls.Add(this.button5, 1, 1);
+            this.board.Controls.Add(this.button2, 1, 0);
+            this.board.Controls.Add(this.button6, 2, 1);
+            this.board.Controls.Add(this.button8, 1, 2);
             this.board.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
             this.board.Location = new System.Drawing.Point(26, 24);
             this.board.Name = "board";
@@ -70,116 +74,6 @@ namespace Puzzle
             this.board.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.board.Size = new System.Drawing.Size(400, 396);
             this.board.TabIndex = 0;
-            // 
-            // title
-            // 
-            this.title.AutoSize = true;
-            this.title.Font = new System.Drawing.Font("Bauhaus 93", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.title.Location = new System.Drawing.Point(486, 24);
-            this.title.Margin = new System.Windows.Forms.Padding(3);
-            this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(276, 57);
-            this.title.TabIndex = 1;
-            this.title.Text = "The Puzzle";
-            // 
-            // timer
-            // 
-            this.timer.AutoSize = true;
-            this.timer.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.timer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.timer.Font = new System.Drawing.Font("Agency FB", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timer.ForeColor = System.Drawing.Color.Green;
-            this.timer.Location = new System.Drawing.Point(566, 104);
-            this.timer.Margin = new System.Windows.Forms.Padding(10);
-            this.timer.Name = "timer";
-            this.timer.Size = new System.Drawing.Size(103, 53);
-            this.timer.TabIndex = 2;
-            this.timer.Text = "00:00";
-            this.timer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // button
-            // 
-            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button.Location = new System.Drawing.Point(531, 194);
-            this.button.Name = "button";
-            this.button.Size = new System.Drawing.Size(181, 47);
-            this.button.TabIndex = 3;
-            this.button.TabStop = false;
-            this.button.Text = "START";
-            this.button.UseVisualStyleBackColor = true;
-            // 
-            // panel
-            // 
-            this.panel.Location = new System.Drawing.Point(514, 274);
-            this.panel.Name = "panel";
-            this.panel.Size = new System.Drawing.Size(218, 133);
-            this.panel.TabIndex = 4;
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(136, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 124);
-            this.button1.TabIndex = 0;
-            this.button1.TabStop = false;
-            this.button1.Text = "1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(269, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(125, 124);
-            this.button2.TabIndex = 1;
-            this.button2.TabStop = false;
-            this.button2.Text = "2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(4, 135);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(125, 124);
-            this.button3.TabIndex = 2;
-            this.button3.TabStop = false;
-            this.button3.Text = "3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(136, 135);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(125, 124);
-            this.button4.TabIndex = 3;
-            this.button4.TabStop = false;
-            this.button4.Text = "4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(269, 266);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(125, 125);
-            this.button5.TabIndex = 4;
-            this.button5.TabStop = false;
-            this.button5.Text = "5";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(4, 4);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(125, 124);
-            this.button6.TabIndex = 5;
-            this.button6.TabStop = false;
-            this.button6.Text = "6";
-            this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
@@ -192,25 +86,175 @@ namespace Puzzle
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 124);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(269, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 124);
+            this.button3.TabIndex = 2;
+            this.button3.TabStop = false;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(4, 135);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 124);
+            this.button4.TabIndex = 3;
+            this.button4.TabStop = false;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(136, 135);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 124);
+            this.button5.TabIndex = 4;
+            this.button5.TabStop = false;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(136, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 124);
+            this.button2.TabIndex = 1;
+            this.button2.TabStop = false;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(269, 135);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(125, 124);
+            this.button6.TabIndex = 5;
+            this.button6.TabStop = false;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
             // button8
             // 
             this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button8.Location = new System.Drawing.Point(269, 135);
+            this.button8.Location = new System.Drawing.Point(136, 266);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(125, 124);
+            this.button8.Size = new System.Drawing.Size(125, 125);
             this.button8.TabIndex = 7;
             this.button8.TabStop = false;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Bauhaus 93", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(486, 24);
+            this.title.Margin = new System.Windows.Forms.Padding(3);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(276, 57);
+            this.title.TabIndex = 1;
+            this.title.Text = "The Puzzle";
+            // 
+            // clock
+            // 
+            this.clock.AutoSize = true;
+            this.clock.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.clock.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.clock.Font = new System.Drawing.Font("Agency FB", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clock.ForeColor = System.Drawing.Color.Green;
+            this.clock.Location = new System.Drawing.Point(538, 94);
+            this.clock.Margin = new System.Windows.Forms.Padding(10);
+            this.clock.Name = "clock";
+            this.clock.Size = new System.Drawing.Size(163, 81);
+            this.clock.TabIndex = 2;
+            this.clock.Text = "00:00";
+            this.clock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(531, 300);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(181, 47);
+            this.start.TabIndex = 3;
+            this.start.TabStop = false;
+            this.start.Text = "START GAME";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // end
+            // 
+            this.end.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.end.Location = new System.Drawing.Point(531, 362);
+            this.end.Name = "end";
+            this.end.Size = new System.Drawing.Size(181, 47);
+            this.end.TabIndex = 4;
+            this.end.TabStop = false;
+            this.end.Text = "END GAME";
+            this.end.UseVisualStyleBackColor = true;
+            this.end.Click += new System.EventHandler(this.end_Click);
+            // 
+            // modes
+            // 
+            this.modes.AllowDrop = true;
+            this.modes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modes.FormattingEnabled = true;
+            this.modes.ItemHeight = 25;
+            this.modes.Items.AddRange(new object[] {
+            "Easy",
+            "Medium",
+            "Hard"});
+            this.modes.Location = new System.Drawing.Point(531, 254);
+            this.modes.Name = "modes";
+            this.modes.ScrollAlwaysVisible = true;
+            this.modes.Size = new System.Drawing.Size(181, 29);
+            this.modes.TabIndex = 6;
+            this.modes.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(532, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "GAME MODE:";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // Puzzle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel);
-            this.Controls.Add(this.button);
-            this.Controls.Add(this.timer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.modes);
+            this.Controls.Add(this.end);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.clock);
             this.Controls.Add(this.title);
             this.Controls.Add(this.board);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -229,9 +273,8 @@ namespace Puzzle
 
         private System.Windows.Forms.TableLayoutPanel board;
         private System.Windows.Forms.Label title;
-        private System.Windows.Forms.Label timer;
-        private System.Windows.Forms.Button button;
-        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label clock;
+        private System.Windows.Forms.Button start;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button6;
@@ -240,6 +283,10 @@ namespace Puzzle
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button end;
+        private System.Windows.Forms.ListBox modes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,7 @@ namespace Puzzle
 {
     public partial class Puzzle : Form
     {
+        private int TotalTime;
         public Puzzle()
         {
             InitializeComponent();
@@ -19,6 +20,62 @@ namespace Puzzle
 
         private void Puzzle_Load(object sender, EventArgs e)
         {
+            this.end.Enabled = false;
+            this.modes.SelectedIndex = 1;
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            start.Enabled = false;
+            modes.Enabled = false;
+            end.Enabled = true;
+
+            //set the time based on difficulty  and start the timer
+            switch (modes.SelectedIndex)
+            {
+                case 0:
+                    this.TotalTime = 600;
+                    break;
+                case 1:
+                    this.TotalTime = 300;
+                    break;
+                case 2:
+                    this.TotalTime = 180;
+                    break;
+                default:
+                    break;
+            }
+            this.timer.Enabled = true;
+
+
+        }
+
+        private void end_Click(object sender, EventArgs e)
+        {
+            end.Enabled = false;
+            start.Enabled = true;
+            modes.Enabled = true;
+            timer.Stop();   
+            clock.Text = "00:00";
+            
+            
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if(this.TotalTime>0)
+            {
+                TotalTime--;
+                int minutes = TotalTime / 60;
+                int seconds = TotalTime - (minutes * 60);
+                clock.Text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            }
+            else
+            {
+                timer.Stop();
+                MessageBox.Show("time is up!");
+            }
 
         }
     }
